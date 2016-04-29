2e34f5822390662360ea3e0d2216aa328e0db3e5
Prabhjyot Singh Sodhi <prabhjyotsingh95@gmail.com>
2016-04-29 17:12:12 +0530

    training final

--- kmeans.py
+++ kmeans.py
@@ -9,7 +9,7 @@ import time
 clusters = {}
 
 def cluster_points(args):
-    x = args[0]
+    X = args[0]
     mu = args[1]
 
     # print  "len of x:" + str(len(x))
@@ -20,83 +20,93 @@ def cluster_points(args):
     # print bestmukey
 
     dist = []
+    for i in range(len(X)):
+        d = []
+        for j in range(len(mu)):
+            d.append(np.linalg.norm(mu[j] - odata[i]))
+        dist.append(d)
 
-    for cen in mu:
-        dist.append(np.linalg.norm(cen - x))
+    out = []
 
-    bestmukey = dist.index(min(dist))
+    for i in range(len(dist)):
+        out.append([X[i], np.argmin(dist[i])])
+    return out
 
-    return bestmukey
-
-    # try:
-
-    # clusters[bestmukey].append(x)
-
-    # from pprint import  pprint
-    # pprint(clusters)
-
-
-    # except:
-    #     clusters[bestmukey] = [x]
-
-    # print len(clusters.keys())
-
-
-def reevaluate_centers():
+def reevaluate_centers(dim):
     newmu = []
 
-    from pprint import  pprint
-    pprint(len(clusters[0]) + len(clusters[1]) + len(clusters[2]) + len(clusters[3]) + len(clusters[4]))
+    from pprint import pprint
+    # pprint(len(clusters[0]) + len(clusters[1]) + len(clusters[2]) + len(clusters[3]) + len(clusters[4]))
 
     keys = sorted(clusters.keys())
 
-    print len(keys)
+    # print len(keys)
 
     for k in keys:
-        newmu.append(np.mean(clusters[k], axis = 0))
+        if(clusters[k] == []):
+            newmu.append(dim*[0.0])
+        else:
+            newmu.append(np.mean(clusters[k], axis = 0))
 
-    print "len newmu: " + str(len(newmu))
-    print newmu
+    # print "len newmu: " + str(len(newmu))
+    # print newmu
 
     return newmu
 
 
 def has_converged(mu, oldmu):
-    return (set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu]))
-
+    flag = 0
+    for i in range(len(mu)):
+        for d in range(len(mu[0])):
+            if(abs(mu[i][d]-oldmu[i][d]) > 0.001):
+                return 0
+    return 1
 
 def find_centers(X, K):
     # Initialize to K random centers
+    random.seed(100)
     oldmu = random.sample(X, K)
     mu = random.sample(X, K)
     it = 0
-    while it < 10 and not has_converged(mu, oldmu):
+    while it < 50:
         it += 1
-
-        print "did  one cycle"
+        for i in range(len(clusters)):
+            clusters[i] = []
 
         oldmu = mu
         # Assign all points in X to clusters
-        arg = [[x, mu] for x in X]
 
-        keys_put = multiprocessing.Pool(processes=8).map(cluster_points, arg)
+        proc = 3
+        arg =[]
+        s = 0
+        add = len(X)/proc
 
-        print "length of keys_put: " + str(len(keys_put))
-        for key in keys_put:
-            clusters[key] = []
+        for i in range(proc-1):
+            arg.append([X[s:s+add], mu])
+            s = s + add
 
-        for i, key in enumerate(keys_put):
-            clusters[key].append(X[i])
+        arg.append([X[s:], mu])
 
+        tn = time.clock()
+
+        pool = multiprocessing.Pool(processes=proc)
+        keys_put = pool.map(cluster_points, arg)
+
+        print "intermediate time: "
+        print time.clock() - tn
+        for clust in keys_put:
+            for point in clust:
+                clusters[point[1]].append(point[0])
+        print it
         # Reevaluate centers
-        mu = reevaluate_centers()
+        mu = reevaluate_centers(len(X[0]))
+        pool.close()
 
     return(mu, clusters)
 # def remove_first(*args):
 
-filename = "data/StarLightCurves_TEST"
+filename = "./data/StarLightCurves_TRAIN"
 odata = genfromtxt(filename, delimiter=',')
-print len(odata)
 
 ts = time.clock()
 
